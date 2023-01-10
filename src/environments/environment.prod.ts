745f78b2cf980f4a6630196cd291bb4106423858
heronet <sirat4757@gmail.com>
2023-01-10 20:30:38 +0600

    fixed production api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'https://connect-t5in5dp7va-uc.a.run.app/api',
   signalRUrl: 'https://connect-t5in5dp7va-uc.a.run.app/hubs',
 };
