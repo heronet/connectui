1d9e37db9cd7234b5c67390fef9f2558d3618a11
heronet <sirat4757@gmail.com>
2023-01-15 18:08:17 +0600

    fixed post creation

--- src/app/posts/create-post/create-post.component.ts
+++ src/app/posts/create-post/create-post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Post } from 'src/app/models/post';
 import { PostsService } from '../posts.service';
@@ -9,13 +9,17 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent {
+  @ViewChild('filesInput', { static: true }) filesInput:
+    | ElementRef<HTMLInputElement>
+    | undefined;
   filesToUpload: File[] = [];
   imageUrls: string[] = [];
   initialImage: string | undefined;
 
   constructor(private postsService: PostsService) {}
-  handleFileInput(e: Event) {
-    let files = (e.target as HTMLInputElement).files;
+  handleFileInput() {
+    let files = this.filesInput!.nativeElement.files;
+
     for (let i = 0; i != files!.length; ++i) {
       // Collect files
       const file = files!.item(i)!;
@@ -24,12 +28,17 @@ export class CreatePostComponent {
       let reader = new FileReader();
       reader.onload = (e) => {
         this.imageUrls.push(e.target!.result as string);
-        this.initialImage = this.imageUrls[0];
+        if (!this.initialImage) this.initialImage = this.imageUrls[0];
       };
       reader.readAsDataURL(file);
     }
   }
-  clearPictures() {}
+  clearPictures() {
+    this.filesToUpload = [];
+    this.imageUrls = [];
+    this.initialImage = undefined;
+    this.filesInput!.nativeElement.value = '';
+  }
   submitPost(form: NgForm) {
     if (!form.value.postText && this.filesToUpload.length === 0) return;
     const data = form.value;
@@ -38,7 +47,10 @@ export class CreatePostComponent {
     this.filesToUpload.forEach((f) => post.append('uploadPhotos', f));
     this.postsService.createPost(post).subscribe({
       error: (err) => console.log(err),
-      complete: () => form.resetForm(),
+      complete: () => {
+        form.resetForm();
+        this.clearPictures();
+      },
     });
   }
 }
